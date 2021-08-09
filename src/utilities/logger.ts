89d013ad3c9c3012c1cf3b4a0ba5e538ebb3251c
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-08-09 18:01:23 +1000

    feat: implement logger middleware

---
+++ src/utilities/logger.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+//logger middleware
+const logger = (
+    req: express.Request,
+    res: express.Response,
+    next: Function
+): void => {
+    let url = req.url;
+    console.log(`${url} was visited`);
+    next();
+};
+
+export default logger;
