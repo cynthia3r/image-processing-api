038c5bd9c02dd960021c594529f25daa85cd590e
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-08-09 18:03:21 +1000

    test: add testsuite and tests for api endpoints

---
+++ src/tests/routes/indexSpec.ts
@@ -0,0 +1,16 @@
+import supertest from 'supertest';
+import app from '../../index';
+
+const request = supertest(app);
+
+// test suite for api endpoint test
+describe('Test endpoint responses', () => {
+    it('gets the api endpoint', async () => {
+        const response = await request.get('/api');
+        expect(response.status).toBe(200);
+    });
+    it('gets the api/images endpoint', async () => {
+        const response = await request.get('/api/images');
+        expect(response.status).toBe(200);
+    });
+});
