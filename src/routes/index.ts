b63d1f27fd582b6ccf275f5642e18710ce5dbfba
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-08-10 15:25:02 +1000

    style: add request and response return type

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import logger from '../utilities/logger';
 
 const routes = express.Router();
 
-routes.get('/', logger, (req, res) => {
+routes.get('/', logger, (req: express.Request, res: express.Response) => {
   res.send('main api route');
 });
 
